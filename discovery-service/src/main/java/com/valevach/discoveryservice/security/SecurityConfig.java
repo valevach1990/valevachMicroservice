557459481f46bb7a3016ee3865c8498cb242c1b6
Dmitry Valevach <Valevachdima23@gmail.com>
2022-12-30 23:31:21 +0300

    Spring Security and Outh2 работают

---
+++ discovery-service/src/main/java/com/valevach/discoveryservice/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.valevach.discoveryservice.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${eureka.username}")
+    private String userName;
+    @Value("${eureka.password}")
+    private String userPassword;
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(userName)
+                .password(userPassword)
+                .authorities("USER");
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
